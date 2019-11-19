9ccc74e72bca124d3f5f54f5e268e61e54c96feb
Winn Leong <winnleong@MacBook-Air.local>
2019-11-20 00:48:22 +0800

    Update
    ios auto changes

--- lib/services/api/api.g.dart
+++ lib/services/api/api.g.dart
@@ -21,9 +21,8 @@ class _Api implements Api {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _data = <String, dynamic>{};
-    _data.addAll(params.toJson() ?? <String, dynamic>{});
     final Response<Map<String, dynamic>> _result = await _dio.request(
-        'GetUserByUserPhonePwd',
+        'GetUserByUserPhonePwd?$params',
         queryParameters: queryParameters,
         options: RequestOptions(
             method: 'GET',
@@ -41,9 +40,8 @@ class _Api implements Api {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _data = <String, dynamic>{};
-    _data.addAll(params.toJson() ?? <String, dynamic>{});
     final Response<Map<String, dynamic>> _result = await _dio.request(
-        'GetUserRegisteredDI',
+        'GetUserRegisteredDI?$params',
         queryParameters: queryParameters,
         options: RequestOptions(
             method: 'GET',
@@ -52,6 +50,44 @@ class _Api implements Api {
             baseUrl: baseUrl),
         data: _data);
     final value = UserRegisteredDiResponse.fromJson(_result.data);
+    return Future.value(value);
+  }
+
+  @override
+  checkExistingUser(params) async {
+    ArgumentError.checkNotNull(params, 'params');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<String> _result = await _dio.request(
+        'GetUserByUserPhone?$params',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = _result.data;
+    return Future.value(value);
+  }
+
+  @override
+  register(params) async {
+    ArgumentError.checkNotNull(params, 'params');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(params.toJson() ?? <String, dynamic>{});
+    final Response<String> _result = await _dio.request('CreateAppAccount',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'POST',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = _result.data;
     return Future.value(value);
   }
 }
