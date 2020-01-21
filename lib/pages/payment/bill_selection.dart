80c8dac2c573359b7679d8d53c3d28b244bb6624
WinnLeong <wl.ong@outlook.com>
2020-01-21 17:55:00 +0800

    Update
    Add FutureBuilders in bill_selection.dart

--- lib/pages/payment/bill_selection.dart
+++ lib/pages/payment/bill_selection.dart
@@ -1,8 +1,18 @@
+import 'package:epandu/services/repo/bill_repo.dart';
 import 'package:epandu/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class BillSelection extends StatelessWidget {
   final primaryColor = ColorConstant.primaryColor;
+  final billRepo = BillRepo();
+
+  Future _getTelco(context) async {
+    await billRepo.getTelco(context: context);
+  }
+
+  Future _getService(context) async {
+    await billRepo.getService(context: context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +31,28 @@ class BillSelection extends StatelessWidget {
           elevation: 0,
           backgroundColor: Colors.transparent,
         ),
-        body: Container(),
+        body: Column(
+          children: <Widget>[
+            FutureBuilder(
+              future: _getTelco(context),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                if (snapshot.hasData) {
+                  return Container();
+                }
+                return Container();
+              },
+            ),
+            FutureBuilder(
+              future: _getService(context),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                if (snapshot.hasData) {
+                  return Container();
+                }
+                return Container();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
