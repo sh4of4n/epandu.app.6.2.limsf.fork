d2d9905a5ae0f5c8833005c124a915ca6a8e319a
tbsdev8789 <tbsdev8789@gmail.com>
2021-02-02 18:02:40 +0800

    2.0.103 - Update GetUnreadNotificationCount

--- lib/common_library/services/repository/inbox_repository.dart
+++ lib/common_library/services/repository/inbox_repository.dart
@@ -36,14 +36,16 @@ class InboxRepo {
     return Response(false, message: 'No records found.');
   }
 
-  Future<Response> getUnreadNotificationCount({context}) async {
+  Future<Response> getUnreadNotificationCount() async {
     String caUid = await localStorage.getCaUid();
     String caPwd = await localStorage.getCaPwdEncode();
     String userId = await localStorage.getUserId();
     String appId = appConfig.appId;
+    String lastMsgDoc = await localStorage.getMsgDoc();
+    String lastMsgRef = await localStorage.getMsgRef();
 
     String path =
-        'wsCodeCrypt=${appConfig.wsCodeCrypt}&caUid=$caUid&caPwd=$caPwd&appId=$appId&userId=$userId';
+        'wsCodeCrypt=${appConfig.wsCodeCrypt}&caUid=$caUid&caPwd=$caPwd&appId=$appId&userId=$userId&lastMsgDoc=$lastMsgDoc&lastMsgRef=$lastMsgRef';
 
     var response = await networking.getData(
       path: 'GetUnreadNotificationCount?$path',
